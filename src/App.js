15dee50fe65311e32c953817ab6e0a59e994e89c
Ben Lmsc <exanova.labs@icloud.com>
2021-07-31 19:16:53 +0200

    change env

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - {process.env.MYKEY}
+          Learn React - {process.env.REACT_APP_MY_KEY}
         </a>
       </header>
     </div>
