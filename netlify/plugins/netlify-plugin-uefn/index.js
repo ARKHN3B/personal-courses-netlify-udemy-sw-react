bc0e787319eb534a8a2ddcf51ef477ce234a8134
Ben Lmsc <exanova.labs@icloud.com>
2021-08-01 08:56:53 +0200

    test string plugin

--- netlify/plugins/netlify-plugin-uefn/index.js
+++ netlify/plugins/netlify-plugin-uefn/index.js
@@ -1,6 +1,12 @@
 module.exports = {
   onPreBuild: ({ inputs }) => {
-    console.debug({ inputs, env: process.env });
+    const {prefix, def} = inputs;
+
+    if (!def && !def.length) return;
+
+    const definitions = Array.isArray(def) ? def : def.split(/\s*[;|,|\s]\s*/);
+
+    console.debug({ inputs, definitions, env: process.env });
     process.env["REACT_APP_RHINOSHIELD"] = "Les meilleurs coques!";
   }
 }
