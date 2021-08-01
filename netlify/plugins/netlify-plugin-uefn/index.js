6bb7f2dac19b5bf81d76ed1539ba940cdb5c625f
Ben Lmsc <exanova.labs@icloud.com>
2021-08-01 10:09:24 +0200

    Test plugins

--- netlify/plugins/netlify-plugin-uefn/index.js
+++ netlify/plugins/netlify-plugin-uefn/index.js
@@ -7,7 +7,8 @@ module.exports = {
     const definitions = Array.isArray(def) ? def : def.split(/\s*[;|,|\s]\s*/);
 
     for (const definition of definitions) {
-      const key = prefix ? `${prefix}_${definition}` : definition;
+      // Use old concat to provide a support to old Node versions
+      const key = prefix ? (prefix + "_" + definition) : definition;
       const value = process.env[definition];
       process.env[key] = value;
     }
