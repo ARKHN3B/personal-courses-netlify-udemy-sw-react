af61ac6a0e2a0e71d7f018fe58a6656e6c18da7d
Ben Lmsc <exanova.labs@icloud.com>
2021-08-02 08:27:39 +0200

    test form

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <form name="contact" data-netlify={true}>
+        {/* A little help for the Netlify post-processing bots */}
+        <form name="contact" netlify netlify-honeypot="bot-field">
+          <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>Name <input type="text" name="name" /></label>
           </p>
@@ -28,13 +30,6 @@ function App() {
           rel="noopener noreferrer"
         >
           My default branch {process.env.REACT_APP_BIG_ENV}<br/>
-          <ol>
-            <li>(((Test env from netlify deploy settings))): ===> {process.env.REACT_APP_MY_KEY}</li>
-            <li>(((Test env from netlify TOML file for the whole environments))): ===> {process.env.REACT_APP_TEST_KEY_FROM_NETLIFY_TOML_ENV}</li>
-            <li>(((Test env from netlify TOML file only for production))): ===> {process.env.REACT_APP_TEST_ENV_ONLY_PROD} {process.env.TEST_ENV_ONLY_PROD}</li>
-            <li>(((Test env from netlify TOML file only for deploy branch))): ===> {process.env.REACT_APP_TEST_ENV_DEPLOY_BRANCH}</li>
-            <li>(((Test env from netlify TOML file for ALL (overrides)))): ===> {process.env.REACT_APP_BIG_ENV}</li>
-          </ol>
         </a>
       </header>
     </div>
